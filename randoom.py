1821f84bc8c90a6f0b793b10863d39b1151fa195
thomphil <tom.phillimore@googlemail.com>
2023-08-14 09:08:23 +0100

    change to gpt4 model

--- randoom.py
+++ randoom.py
@@ -20,7 +20,7 @@ def get_starting_convo():
 def call_openai_api(conversation):
     st.spinner("The code is being generated...")
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=conversation, temperature=0.1
+        model="gpt-4", messages=conversation, temperature=0.1
     )
     return response["choices"][0]["message"]["content"].strip()
 
